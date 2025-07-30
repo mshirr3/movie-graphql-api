b2399501f299825abf2210f79c0b7bdd2a975099
Mohamed Shire <ms227te@student.lnu.se>
2025-07-31 01:28:18 +0200

    resolver func for fetching specific movie + little error handling

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,14 +1,42 @@
+import { GraphQLError } from 'graphql'
 import Movie from './models/movie.js'
 
 const resolvers = {
     Query: {
         // returns an array of movies
          movies: async (_, { genre, release_year }) => {
-            const filter = {}
-            if (genre) { filter.genre = genre}
-            if (release_year) {filter.release_year = release_year}
-            return await Movie.find(filter)
+            try {
+              const filter = {}
+              if (genre) { filter.genre = genre}
+              if (release_year) {filter.release_year = release_year}
+              return await Movie.find(filter)
+            } catch (error) {
+                throw new GraphQLError('Failed to fetch movies', {
+                    extensions: {code: 'INTERNAL_SERVER_ERROR',error: error.message }
+                })
+            }
+        },
+         movie: async (_, { id }) => {
+            try {
+              const movie = await Movie.findOne({id})
+              if (!movie) {
+                throw new GraphQLError('Movie not found', {
+                    extensions: {
+                        code:'NOT_FOUND',
+                        http: { status: 404 }
+                    }
+                })
+            }
+              return movie
+            } catch (error) {
+              throw new GraphQLError('Failed to fetch movie', {
+                extensions: {
+                    code: 'INTERNAL_SERVER_ERROR',
+                    error: error.message
+                }
+            })
         }
+         }
     }
 }
 
